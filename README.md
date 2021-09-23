a807069f18218f198808c784b0476b52696ed9d1
bhagya2002 <bhagya02.p@gmail.com>
2021-09-23 08:25:28 -0600

    formatting updates for easier use

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # instagramPlus
- Instagram webscrapper to get a list of followers and followees in seperated files.
+
+Instagram webscrapper to get a list of followers and followees in seperated files.
+
+## Instructions
+
+1. Replace "USERNAME" and "PASSWORD" with your username and password.
+2. Run instagram.py first, this program will make two new files (followers and followees)
+   Note: this will take some time to go through your follower list and finsih writing to the files
+3. Run ig_checker.py second, this program goes through both new txt files and check which usernames arent in both them makes a new list
+
+Now you can see who doesnt follow you back.
--- instagram.py
+++ instagram.py
@@ -3,24 +3,24 @@ import instaloader
 L = instaloader.Instaloader()
 
 # Login or load session
-L.login("USERNAME", "PASSWORD") # (login)
+L.login('USERNAME', "PASSWORD") # (login)
 
 
 # Obtain profile metadata
-profile = instaloader.Profile.from_username(L.context, "PROFILE USER")
+profile = instaloader.Profile.from_username(L.context, "USERNAME")
 
 # Print list of followees (who follows me)
-    # file = open("FILENAME","w")
-    # for follower in profile.get_followers():
-    #     username = follower.username
-    #     file.write(username + "\n")
+file = open("followees.txt","w")
+for follower in profile.get_followers():
+    username = follower.username
+    file.write(username + "\n")
 
-    # file.close()
+file.close()
 
-# Print list of followees (who i follow)
-    # file = open("FILENAME.txt","a+")
-    # for followee in profile.get_followees():
-    #     username = followee.username
-    #     file.write(username + "\n")
-
-    # file.close()
+# Print list of followers (who i follow)
+file = open("followers.txt","a+")
+for followee in profile.get_followees():
+    username = followee.username
+    file.write(username + "\n")
+        
+file.close()
